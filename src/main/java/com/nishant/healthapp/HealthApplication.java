056d24359c1993507e9c9289c6797c7ac212e266
CHAUHANN <CHAUHANN@CHAUHANN01.corp.amdocs.com>
2018-07-17 07:56:37 -0400

    Test Git Eclipse

--- src/main/java/com/nishant/healthapp/HealthApplication.java
+++ src/main/java/com/nishant/healthapp/HealthApplication.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 /**
  * @author Nishant Chauhan
  * */
+
 @SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
 @Controller
 public class HealthApplication extends SpringBootServletInitializer {
